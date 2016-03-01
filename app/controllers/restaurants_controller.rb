3f84fdf551e29c376ecd04934bf2f834f04ae195
Tom Pickard <tcpickard94@gmail.com>
2016-03-01 11:30:38 +0000

    User Story 1 Completed
    Added show, new and create methods to restaurant controller
    Created the restaurant model
    Created view for adding new restaurants and displaying all
    Updated database.yml so we are now using postgres
    Removed sqlite from gemfile and replaced with pg
    Passed first feature test

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,24 @@
 class RestaurantsController < ApplicationController
   def index
   end
+  
+  def show
+    @restaurants = Restaurant.all
+  end
+
+  def new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :rating)
+  end
+ 
 end
