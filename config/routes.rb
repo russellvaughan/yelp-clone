b483550c466857c4f5bfa26844c8445e2979cf82
Godfrey Chiu <ggwc82@gmail.com>
2016-03-02 23:55:41 +0000

    Implemented Rails gem devise for user sign up, in and out. Added tests to check these features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,8 +9,8 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  
 
+  root to: "restaurants#index"
 
   resources :restaurants do
     resources :reviews
