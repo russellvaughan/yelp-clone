8bba9fa3fd9119c27647ca89b24a347ec20eeb4a
Iryna Howarth <irynahowarth@gmail.com>
2016-03-02 17:15:01 +0000

    set up devise/ add links sign-in and sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+devise_for :users
+
 resources :restaurants do
   resources :reviews
 end
 
-get 'restaurants' => 'restaurants#index'
+# get 'restaurants' => 'restaurants#index'
+
+root 'restaurants#index'
 
 end
