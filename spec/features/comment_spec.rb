b48fbdc6c7841ccf3f7550a66043fe72fcde0a13
russellvaughan <russellvaughan@gmail.com>
2016-03-02 07:58:36 +0000

    fixed delete bug by adding :dependent => :delete_all to restaurant model

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -9,3 +9,20 @@ feature 'User can review restaurant' do
     expect(page).to have_content("Average, 3")
   end
 end
+
+feature 'Page features all reviews' do
+  scenario 'When user visits restaurant page' do
+    create_restaurant
+    click_link "Nando's"
+    expect(current_path).to eq('/restaurants/1')
+    fill_in "Comment", with: "Average"
+    fill_in "Rating", with: 3
+    click_button "Submit Review"
+    expect(current_path).to eq('/restaurants/1')
+    fill_in "Comment", with: "Great"
+    fill_in "Rating", with: 5
+    click_button "Submit Review"
+    expect(page).to have_content("Average, 3 Great, 5")
+  end
+end
+
