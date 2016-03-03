b9a703491b194212b942fa82cea2888a13a9eee3
russellvaughan <russellvaughan@gmail.com>
2016-03-03 15:52:19 +0000

    facebook login now a success

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -23,8 +24,9 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
-config.omniauth :facebook, "1587310631592788", Rails.application.secrets.facebook_app_secret,
-                callback_url: "http://localhost:3000"
+config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
+
+
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
