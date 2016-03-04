45066fc4160a7f6fb5cff2e047978f06b961ca7d
russellvaughan <russellvaughan@gmail.com>
2016-03-04 09:16:54 +0000

    user cannot review same restaurant more than onece

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,9 +7,16 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create(review_params)
-
-    redirect_to restaurants_path
+    @review = @restaurant.build_review review_params,current_user
+    if @review.save
+      redirect_to restaurants_path
+    else
+    if @review.errors[:user]
+      redirect_to restaurants_path, alert:"you have already reviewed this restaurant"
+    else
+      render :new  
+    end
+  end
   end
 
   def review_params
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,9 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   belongs_to :user
   validates :name, length: {minimum: 3}, uniqueness: true
+  
+  def build_review(attributes={}, user)
+  attributes[:user] ||= user 
+  reviews.build(attributes)
+  end
 end
